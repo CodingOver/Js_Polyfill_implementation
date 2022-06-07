fdd08dee4202364c4cceced49c81372634adc14a
CodingOver <paypal1business.over@gmail.com>
2022-06-07 23:47:46 +0100

    implment next() iterator Object for entries() method

--- Arrays/entries.js
+++ Arrays/entries.js
@@ -1,5 +1,18 @@
-function arrayIterator(array, kind) {
+function ArrayIterator(array, kind) {
     this.array = array;
     this.kind = kind;
     this.nextIndex = 0
 }
+
+
+ArrayIterator.prototype.next = function () {
+    var currentIndex = this.nextIndex;
+
+    if (currentIndex >= this.array.length) {
+        return { 'value': undefined, 'done': true }
+    }
+}
+
+// const array1 = ['a', 'b', 'c', 3];
+
+// console.log(test)

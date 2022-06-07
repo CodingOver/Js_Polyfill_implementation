e18fc60659b40b03968de29e8f5ea5acb6cec321
CodingOver <paypal1business.over@gmail.com>
2022-06-08 00:55:40 +0100

    update entries() method

--- Arrays/entries.js
+++ Arrays/entries.js
@@ -1,29 +1,41 @@
 function ArrayIterator(array, kind) {
     this.array = array;
     this.kind = kind;
-    this.nextIndex = 0
+    this.nextIndex = 0;
 }
-
 
 ArrayIterator.prototype.next = function () {
     var currentIndex = this.nextIndex;
 
     if (currentIndex >= this.array.length) {
-        return { 'value': undefined, 'done': true }
+        return { 'value': undefined, "done": true }
     }
-    this.nextIndex++;
 
-    // Iterating entries()
+    this.nextIndex++
+
+
     if (this.kind === 'key+value') {
         return {
             'value': [currentIndex, this.array[currentIndex]],
             'done': false
-        }
-    }
-    Array.prototype.myEntries = function () {
-        return new ArrayIterator(this, 'key+value')
-    }
+        };
+    };
+
+};
+
+Array.prototype.myEntries = function () {
+    return new ArrayIterator(this, 'key+value')
 }
 
-const array1 = ['a', 'b', 'c']
-console.log(array1.myEntries())
+
+// let arr = [1, 2, 3, 4];
+
+// let iterator = arr.myEntries();
+
+
+// console.log(iterator.next().value)
+// console.log(iterator.next().value)
+// console.log(iterator.next().value)
+// console.log(iterator.next().value)
+
+// console.log(iterator.next().value) // undefiend

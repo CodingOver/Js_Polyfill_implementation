5133da6e95b066f0077b3361f4c04a69dbe8a0a0
CodingOver <paypal1business.over@gmail.com>
2022-06-08 00:00:10 +0100

    implmet entries() methode

--- Arrays/entries.js
+++ Arrays/entries.js
@@ -11,8 +11,19 @@ ArrayIterator.prototype.next = function () {
     if (currentIndex >= this.array.length) {
         return { 'value': undefined, 'done': true }
     }
+    this.nextIndex++;
+
+    // Iterating entries()
+    if (this.kind === 'key+value') {
+        return {
+            'value': [currentIndex, this.array[currentIndex]],
+            'done': false
+        }
+    }
+    Array.prototype.myEntries = function () {
+        return new ArrayIterator(this, 'key+value')
+    }
 }
 
-// const array1 = ['a', 'b', 'c', 3];
-
-// console.log(test)
+const array1 = ['a', 'b', 'c']
+console.log(array1.myEntries())
